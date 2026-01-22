dbbbc247ab038346ae9509507e72760876be0a46
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2026-01-22 19:48:35 +0100

    fix bug moe sparsity

--- src/model/moe.py
+++ src/model/moe.py
@@ -26,16 +26,15 @@ class MoE(nn.Module):
         assert D == self.d_model
 
         logits = self.gate(x)  # (B,T,E)
-        gate = F.softmax(logits, dim=-1)  # (B,T,E)
+        
+        top_val, top_idx = torch.topk(logits,k=self.top_k,dim=-1) # both (B,T,K)
 
-        top_val, top_idx = torch.topk(gate, k=self.top_k, dim=-1)  # both (B,T,K)
+        top_w = nn.functional.softmax(top_val,dim=-1)  # (B,T,K)
 
-        top_w = top_val / (top_val.sum(dim=-1, keepdim=True) + 1e-9)  # (B,T,K)
+        W_selected = self.We[top_idx] # (B,T,K,D,D)
 
-        x_experts = torch.einsum("btd,edm->btem", x, self.We)
+        x_experts = torch.einsum("btd,btkdm->btkm",x,W_selected)
 
-        idx = top_idx.unsqueeze(-1).expand(B, T, self.top_k, D)  # (B,T,K,D)
-        x_sel = torch.gather(x_experts, dim=2, index=idx)  # (B,T,K,D)
+        out = (x_experts * top_w.unsqueeze(-1)).sum(dim=2)
 
-        out = (x_sel * top_w.unsqueeze(-1)).sum(dim=2)  # (B,T,D)
         return out
